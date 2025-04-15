84088a31cc55944ab3d0cf6e06fe38c2293158b8
Rotimi <rotimimichaeljames@gmail.com>
2025-04-15 07:08:53 -0700

    Mr Rotimi Committe today

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,8 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const redis = new Redis(process.env.REDIS_URI);
+console.log("REDIS_URL:", process.env.REDIS_URL);
+
 
 redis.on("connect", () => console.log("✅ Redis Connected Successfull...!"));
 redis.on("error", (err) => console.error("❌ Redis Error:", err));
+
 
 export default redis;
