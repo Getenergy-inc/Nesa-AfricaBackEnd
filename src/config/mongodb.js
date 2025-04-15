b38db7ba8677b1b2e071c2d5ae575ce955664956
Rotimi <rotimimichaeljames@gmail.com>
2025-04-15 06:14:38 -0700

    Mr Rotimi Committe today

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -4,17 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectMongoDB = async () => {
-
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // Simplified
     console.log("✅ MongoDB Connected Successfully...");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error);
   }
-  
 };
 
 export default connectMongoDB;
--- src/config/redis.js
+++ src/config/redis.js
@@ -1,11 +1,11 @@
-// import Redis from "ioredis";
-// import dotenv from "dotenv";
+import Redis from "ioredis";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const redis = new Redis(process.env.REDIS_URI);
+const redis = new Redis(process.env.REDIS_URI);
 
-// redis.on("connect", () => console.log("✅ Redis Connected Successfull...!"));
-// redis.on("error", (err) => console.error("❌ Redis Error:", err));
+redis.on("connect", () => console.log("✅ Redis Connected Successfull...!"));
+redis.on("error", (err) => console.error("❌ Redis Error:", err));
 
-// export default redis;
+export default redis;
