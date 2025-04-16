0a4549e633f35dd33b97e1b0807868eac46bb857
Rotimi <rotimimichaeljames@gmail.com>
2025-04-16 01:51:28 -0700

    Mr Rotimi Committe today

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -4,12 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectMongoDB = async () => {
+
   try {
-    await mongoose.connect(process.env.MONGO_URI); // Simplified
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: false,
+      useUnifiedTopology: false,
+    });
     console.log("✅ MongoDB Connected Successfully...");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error);
   }
+  
 };
 
 export default connectMongoDB;
