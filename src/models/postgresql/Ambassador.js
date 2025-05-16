b62c884314018fef14741e85d9ea4102e5ef8900
Rotimi <rotimimichaeljames@gmail.com>
2025-05-16 03:31:05 -0700

    Mr Rotimi Committe today now

--- src/models/postgresql/Ambassador.js
+++ src/models/postgresql/Ambassador.js
@@ -1,13 +1,44 @@
 import { DataTypes } from "sequelize";
-import {sequelize} from "../../config/database.js";
+import { sequelize } from "../../config/database.js";
 
 const Ambassador = sequelize.define("Ambassador", {
-
-  ambassador_id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  region: { type: DataTypes.STRING, allowNull: false },
-  type: { type: DataTypes.ENUM("project", "senior"), allowNull: false },
-  assigned_KPIs: { type: DataTypes.JSON },
-  
+  ambassador_id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  ageRange: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  phoneNo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  socials: {
+    type: DataTypes.JSON,
+    allowNull: true  // Example: { instagram: "", twitter: "", linkedin: "" }
+  }
 });
 
 export default Ambassador;
